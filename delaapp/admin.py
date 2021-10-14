5bf1c2644d33281059c5e97210c7c79114f88a4e
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-14 16:28:14 +0300

    register models in admin

--- delaapp/admin.py
+++ delaapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from delaapp.models import Comment, Image, Profile
+
+
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Profile)
--- delagram/settings.py
+++ delagram/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'delagram.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'delagram',
+        'USER': 'mikeywalked',
+        'PASSWORD':'practice',        
+    }    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -106,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
