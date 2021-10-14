44af289ef15c2d4d7f179a37ec82ba70f984ab92
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-14 13:58:52 +0300

    make profile class

--- delaapp/models.py
+++ delaapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE,)
+  dp = models.ImageField(upload_to = 'profiles/', default='SOME STRING')
+  bio = models.TextField(max_length=500, default=f'This is my bio, Welcome!')
+  def __str__(self):
+         return self.user.username
+
+  # def save_profile(self):
+  #        self.save()
+  # def delete_profile(self):
+  #        self.delete()     
+
+# class Image(models.Model):
+#   image = models.ImageField(upload_to='images/')
+#   caption = models.TextField()
+#   owner = models.ForeignKey('Profile', on_delete=models.CASCADE)
+#   likes = models.ManyToManyField(User, blank=True)
+#   pub_date = models.DateTimeField(auto_now_add=True)
+
+#   def __str__(self):
+#     return self.caption
+    
+
+#   def updater(self, cap):
+#     try:
+#       self.caption = cap
+#       self.save()
+#       return self
+#     except self.DoesNotExist:
+#       print('The caption does not exist in our records')
+
+# class Comment(models.Model):
+#   postde = models.ForeignKey('Image', on_delete=models.CASCADE)
+#   owner = models.ForeignKey('Profile', on_delete=models.CASCADE)
+#   content = models.TextField()
+#   pub_date = models.DateTimeField(auto_now_add=True)
+
+#   def __str__(self):
+#     return self.content  
+
+# class Foll(models.Model):
+#     target = models.ForeignKey('User', related_name='followers')
+#     follower = models.ForeignKey('User', related_name='targets')
+#     def __str__(self):
+#      return self.target
