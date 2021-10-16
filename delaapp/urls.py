777ca86a001d52dfa1ceb8dd394ae055cbf54ba7
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-16 11:23:26 +0300

    add profile url to urls.py

--- delaapp/urls.py
+++ delaapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url('^$',views.landing,name='home'),  
     url(r'^new/post$', views.new_post, name='new_post'),
     url(r'^comments/(\d+)', views.comment, name='comment'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
 
 ]
