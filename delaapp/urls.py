04204e81f855fec80741826188b8a41d099b42ba
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-16 11:23:01 +0300

    add comment url to urls.py

--- delaapp/urls.py
+++ delaapp/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 urlpatterns = [
     url('^$',views.landing,name='home'),  
     url(r'^new/post$', views.new_post, name='new_post'),
- 
+    url(r'^comments/(\d+)', views.comment, name='comment'),
+
 ]
