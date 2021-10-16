185913b1ed7af2f6e25635a2635173a19667015b
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-16 11:23:49 +0300

    add you url to urls.py

--- delaapp/urls.py
+++ delaapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^new/post$', views.new_post, name='new_post'),
     url(r'^comments/(\d+)', views.comment, name='comment'),
     url(r'^profile/(\d+)', views.profile, name='profile'),
-
+    url(r'^you/', views.you, name="you"), 
+ 
 ]
