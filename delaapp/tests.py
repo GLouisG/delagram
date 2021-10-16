a157d98618de6234b1344adbb26e112d21b26b62
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-16 10:35:42 +0300

    add UserProfile test class to tests

--- delaapp/tests.py
+++ delaapp/tests.py
@@ -1,3 +1,16 @@
+from datetime import datetime
+from django.contrib.auth.models import User
 from django.test import TestCase
 
+from delaapp.models import Comment, Image, Profile
+
 # Create your tests here.
+class UserProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="Johnny")
+        self.profile = Profile(user=self.user, picture="test.jpg", bio = "A description" )    
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
