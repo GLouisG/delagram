fd2d18361d5118953901554edd433ff6280593a5
GLouisG <louis.gacho@student.moringaschool.com>
2021-10-16 10:51:15 +0300

    fix img_searcher tests

--- delagram/settings.py
+++ delagram/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'delaapp.apps.DelaappConfig',    
+    'delaapp.apps.DelaappConfig', 
+    'django.contrib.postgres'   
 ]
 
 MIDDLEWARE = [
